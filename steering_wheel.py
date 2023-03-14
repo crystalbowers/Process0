743fe40b8855171331ea6f4f82b8df5fca087095
Crystal Bowers <angelbowers19@gmail.com>
2023-03-13 23:43:26 -0500

    updated steering wheel effects

--- steering_wheel.py
+++ steering_wheel.py
@@ -4,18 +4,27 @@ import adafruit_drv2605
 i2c = busio.I2C(board.SCL, board.SDA)
 drv = adafruit_drv2605.DRV2605(i2c)
 
-drv.sequence[0] = adafruit_drv2605.Effect(1)
-print("sharp click")
+#drv.sequence[0] = adafruit_drv2605.Effect(1)
+#print("sharp click")
 
-drv.play()
-drv.sequence[0] = adafruit_drv2605.Effect(47)
-drv.play()
-print("strong buzz")
+#drv.play()
+#drv.sequence[0] = adafruit_drv2605.Effect(47)
+#drv.play()
+#print("strong buzz")
 
-drv.sequence[0] = adafruit_drv2605.Effect(1)
+effect_num=118
+drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
 drv.sequence[1] = adafruit_drv2605.Pause(0.5)
-drv.sequence[2] = adafruit_drv2605.Effect(47)
+drv.sequence[2] = adafruit_drv2605.Effect(effect_num)
 drv.sequence[3] = adafruit_drv2605.Effect(0)
 drv.play()
 print("Those two effects with pauses")
-drv.stop()
+#drv.stop()
+# We like 83. 106 is better, slightly stronger than 82
+# 70 is wierd
+# 118 is strongest but won't stop
+
+# sda 3
+# scl 5
+# ground ground
+# bus 1
