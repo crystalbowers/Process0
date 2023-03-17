108f43a0d261c598a0a99cb9701a2c3dc6bff291
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 12:38:39 -0500

    Testing w/steering wheel code

--- steering_wheel.py
+++ steering_wheel.py
@@ -6,14 +6,6 @@ import time
 i2c = busio.I2C(board.SCL, board.SDA)
 drv = adafruit_drv2605.DRV2605(i2c)
 
-#drv.sequence[0] = adafruit_drv2605.Effect(1)
-#print("sharp click")
-
-#drv.play()
-#drv.sequence[0] = adafruit_drv2605.Effect(47)
-#drv.play()
-#print("strong buzz")
-
 #effect_num=118
 #drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
 #drv.sequence[1] = adafruit_drv2605.Pause(0.5)
@@ -21,14 +13,16 @@ drv = adafruit_drv2605.DRV2605(i2c)
 #drv.sequence[3] = adafruit_drv2605.Effect(0)
 #drv.play()
 #print("Those two effects with pauses")
+def steering_wheel_vibrate():
+    start = time.time()
+    while time.time() < start + 5:
+        effect_num = 118
+        drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
+        #drv.sequence[1] = adafruit_drv2605.Pause(0.5)
+        drv.play()  # play the effect
+        #time.sleep(0.5)  # for 0.5 seconds
+        print("Running steering wheel vibration")
 
-effect_num=118
-drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
-drv.sequence[1] = adafruit_drv2605.Pause(0.5)
-drv.play()  # play the effect
-time.sleep(0.5)  # for 0.5 seconds
-drv.stop()  # and then stop (if it's still running)
-print("Those two effects with pauses")
 #drv.stop()
 # We like 83. 106 is better, slightly stronger than 82
 # 70 is wierd
@@ -38,3 +32,12 @@ print("Those two effects with pauses")
 # scl 5
 # ground ground
 # bus 1
+
+
+#drv.sequence[0] = adafruit_drv2605.Effect(1)
+#print("sharp click")
+
+#drv.play()
+#drv.sequence[0] = adafruit_drv2605.Effect(47)
+#drv.play()
+#print("strong buzz")
