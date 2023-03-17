b345f6e5cb84ab97bd83ca71b68e38b6f1558434
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 13:21:35 -0500

    Testing w/steering wheel code

--- steering_wheel.py
+++ steering_wheel.py
@@ -7,9 +7,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 drv = adafruit_drv2605.DRV2605(i2c)
 
 effect_num=118
-drv.sequence[0] = time.sleep(1)
-drv.sequence[1] = adafruit_drv2605.Effect(effect_num)
-drv.sequence[2] = adafruit_drv2605.Pause(0.5)
+drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
+drv.sequence[1] = adafruit_drv2605.Pause(0.5)
 
 
 #drv.sequence[2] = adafruit_drv2605.Effect(effect_num)
