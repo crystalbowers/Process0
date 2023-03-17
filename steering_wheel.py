54cd1419c1d7b06236ff7662cfdd916da3919bfb
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 12:23:17 -0500

    Testing w/steering wheel code

--- steering_wheel.py
+++ steering_wheel.py
@@ -1,6 +1,8 @@
 import board
 import busio
 import adafruit_drv2605
+import time
+
 i2c = busio.I2C(board.SCL, board.SDA)
 drv = adafruit_drv2605.DRV2605(i2c)
 
@@ -12,12 +14,20 @@ drv = adafruit_drv2605.DRV2605(i2c)
 #drv.play()
 #print("strong buzz")
 
+#effect_num=118
+#drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
+#drv.sequence[1] = adafruit_drv2605.Pause(0.5)
+#drv.sequence[2] = adafruit_drv2605.Effect(effect_num)
+#drv.sequence[3] = adafruit_drv2605.Effect(0)
+#drv.play()
+#print("Those two effects with pauses")
+
 effect_num=118
 drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
 drv.sequence[1] = adafruit_drv2605.Pause(0.5)
-drv.sequence[2] = adafruit_drv2605.Effect(effect_num)
-drv.sequence[3] = adafruit_drv2605.Effect(0)
-drv.play()
+drv.play()  # play the effect
+time.sleep(0.5)  # for 0.5 seconds
+drv.stop()  # and then stop (if it's still running)
 print("Those two effects with pauses")
 #drv.stop()
 # We like 83. 106 is better, slightly stronger than 82
