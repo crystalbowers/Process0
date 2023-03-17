36257f463ead32a04981132f4b60995569d5fa54
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 14:29:07 -0500

    Fixed self.i2c variable error

--- steering_wheel_motor_control.py
+++ steering_wheel_motor_control.py
@@ -7,7 +7,7 @@ class Steering_Wheel_MotorController:
     def __init__(self, effect_num=118):
         # Initalize I2C bus and DRV2065 module
         self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.drv = adafruit_drv2605.DRV2605(i2c)
+        self.drv = adafruit_drv2605.DRV2605(self.i2c)
 
         # Set effect number
         self.effect_num = effect_num
