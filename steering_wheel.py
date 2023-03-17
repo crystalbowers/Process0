165c1d9f3de7308500284bf5c19d9aeb39c69f0a
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 13:47:07 -0500

    Testing w/steering wheel code

--- steering_wheel.py
+++ steering_wheel.py
@@ -2,15 +2,16 @@ import board
 import busio
 import adafruit_drv2605
 
+# Connect to I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 drv = adafruit_drv2605.DRV2605(i2c)
-print("I'm done setting up")
 
+# Run vibrations
 effect_num = 118
 drv.sequence[0] = adafruit_drv2605.Effect(effect_num)
-drv.sequence[1] = adafruit_drv2605.Pause(1)
-drv.sequence[2] = adafruit_drv2605.Effect(effect_num)
-drv.sequence[3] = adafruit_drv2605.Effect(0)
+#drv.sequence[1] = adafruit_drv2605.Pause(2)
+#drv.sequence[2] = adafruit_drv2605.Effect(effect_num)
+#drv.sequence[3] = adafruit_drv2605.Effect(0)
 drv.play()
 
 print("Those two effects with pauses")
@@ -21,6 +22,7 @@ print("Those two effects with pauses")
 # 70 is wierd
 # 118 is strongest but won't stop
 
+# Wiring Info:
 # sda 3
 # scl 5
 # ground ground
