d3ff60613339fd778f5a16deeceb9a5b95a520c0
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 15:07:55 -0500

    Added comments and changed parameter input to pwm1 instead of pwm

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from seat_motor_control import Seat_MotorController
 from steering_wheel_motor_control import Steering_Wheel_MotorController
 
 # Running seat cushion
-#seat_mc = Seat_MotorController(pwm_pin=32, pwm_freq=50)
+#seat_mc = Seat_MotorController()
 #seat_mc.run(side="left", delay_secs=10)
 #seat_mc.stop_motors()
 
