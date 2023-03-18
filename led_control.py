7603a30817a7cd47d509001266e54aaa50446fac
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 19:58:29 -0500

    LED light code

---
+++ led_control.py
@@ -0,0 +1,21 @@
+import Jetson.GPIO
+import time
+
+class LED_Controller:
+    # Initialize the class and pick your pins for each LED
+    def __init__(self, left_led=7, right_led=21):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(left_led, GPIO.OUT)
+        GPIO.setup(right_led, GPIO.OUT)
+
+    def run_left_side(self):
+        GPIO.output(left_led, GPIO.HIGH)
+        time.sleep(3)
+
+    def run_right_side(self):
+        GPIO.output(right_led, GPIO.HIGH)
+        time.sleep(3)
+
+    def run(self, side="both"):
+        self.run_left_side()
+        self.run_right_side()
--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from seat_motor_control import Seat_MotorController
 from steering_wheel_motor_control import Steering_Wheel_MotorController
-
+from led_control import LED_Controller
 # Running seat cushion
 #seat_mc = Seat_MotorController()
 #seat_mc.run(side="left", delay_secs=10)
 #seat_mc.stop_motors()
 
 # Running Steering Wheels
-steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
-steering_wheel_mc.run()
+#steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
+#steering_wheel_mc.run()
+
+steering_wheel_lc = LED_Controller()
+steering_wheel_lc.run(side=both)
