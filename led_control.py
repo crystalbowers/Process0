d6dea82942fa9e7c463927f7ce3a456a17398d79
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 21:01:32 -0500

    new troubleshooting

--- led_control.py
+++ led_control.py
@@ -2,25 +2,6 @@ import Jetson.GPIO as GPIO
 import time
 
 class LED_Controller:
-    '''def __init__(self):
-        left_led = 7
-        right_led = 21
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(left_led, GPIO.OUT)
-        GPIO.setup(right_led, GPIO.OUT)
-        x = 1
-        while x < 5:
-            time.sleep(2)
-            GPIO.output(left_led, GPIO.HIGH)
-            GPIO.output(right_led, GPIO.HIGH)
-            print("LED IS ON")
-            time.sleep(2)
-            GPIO.output(left_led, GPIO.LOW)
-            GPIO.output(right_led, GPIO.LOW)
-            print("LED IS OFF")
-            x += 1
-            print(x)'''
-class LED_Controller:
     # Initialize the class and pick your pins for each LED
     def __init__(self, left_led=7, right_led=21):
         self.left_led = left_led
