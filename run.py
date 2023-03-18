6cbe79423648a9b6c16df8db522cd076fd4a8f50
Crystal Bowers <angelbowers19@gmail.com>
2023-03-18 13:20:04 -0500

    changed test.py for motor driver test

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 # NOTE: RIGHT NOW SEAT MOTOR CONTROL AND STEERING WHEEL CLASSES EACH TRY TO SET UP GPIO BOARD. Comment out the one you dont want if running both.
-from seat_motor_control import Seat_MotorController
+#from seat_motor_control import Seat_MotorController
 #from steering_wheel_motor_control import Steering_Wheel_MotorController
 #from led_control import LED_Controller
 # Running seat cushion
-seat_mc = Seat_MotorController()
-seat_mc.run(side="left", delay_secs=10)
-seat_mc.stop_motors()
+#seat_mc = Seat_MotorController()
+#seat_mc.run(side="left", delay_secs=10)
+#seat_mc.stop_motors()
 
 # Running Steering Wheel Motors
 #steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
