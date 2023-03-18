712730d0c9249a2d3ca118ed34d12d96d46be2f5
Crystal Bowers <angelbowers19@gmail.com>
2023-03-18 13:23:25 -0500

    testing led soldering

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # NOTE: RIGHT NOW SEAT MOTOR CONTROL AND STEERING WHEEL CLASSES EACH TRY TO SET UP GPIO BOARD. Comment out the one you dont want if running both.
 #from seat_motor_control import Seat_MotorController
 #from steering_wheel_motor_control import Steering_Wheel_MotorController
-#from led_control import LED_Controller
+from led_control import LED_Controller
 # Running seat cushion
 #seat_mc = Seat_MotorController()
 #seat_mc.run(side="left", delay_secs=10)
@@ -11,7 +11,7 @@
 #steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
 #steering_wheel_mc.run()
 
-#steering_wheel_lc = LED_Controller()
-#steering_wheel_lc.run(side="right")
-#steering_wheel_lc.run(side="left")
-#steering_wheel_lc.clean_up()
+steering_wheel_lc = LED_Controller()
+steering_wheel_lc.run(side="right")
+steering_wheel_lc.run(side="left")
+steering_wheel_lc.clean_up()
