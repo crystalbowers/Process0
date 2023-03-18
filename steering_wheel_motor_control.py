a709612b053ba164a346fde8c73a1c22236d39fd
Crystal Bowers <angelbowers19@gmail.com>
2023-03-18 12:29:07 -0500

    deleted i2c print

--- steering_wheel_motor_control.py
+++ steering_wheel_motor_control.py
@@ -6,7 +6,6 @@ import time
 class Steering_Wheel_MotorController:
     def __init__(self, effect_num=118):
         # Initalize I2C bus and DRV2065 module
-        print(self.i2c.scan())
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.drv = adafruit_drv2605.DRV2605(self.i2c)
         # Set effect number
