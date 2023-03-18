7c1cddb8a777eb654d9ad5926559e4fc52d516ff
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 20:50:50 -0500

    updated logic

--- led_control.py
+++ led_control.py
@@ -4,20 +4,22 @@ import time
 class LED_Controller:
     # Initialize the class and pick your pins for each LED
     def __init__(self, left_led=7, right_led=21):
-        self.left_led = left_led
-        self.right_led = right_led
+        #self.left_led = left_led
+        #self.right_led = right_led
         print("done")
-
         GPIO.setmode(GPIO.BOARD)
-        print("eek")
-        GPIO.setup(self.left_led, GPIO.OUT)
-        GPIO.setup(self.right_led, GPIO.OUT)
-        print("ahh")
+        GPIO.setup(left_led, GPIO.OUT)
+        GPIO.setup(right_led, GPIO.OUT)
+
 
     def run_left_side(self):
-        GPIO.output(self.left_led, GPIO.HIGH)
+        left_led=7
+        GPIO.output(left_led, GPIO.HIGH)
         time.sleep(3)
-        GPIO.output(self.left_led, GPIO.LOW)
+        GPIO.output(left_led, GPIO.LOW)
+        #GPIO.output(self.left_led, GPIO.HIGH)
+        #time.sleep(3)
+        #GPIO.output(self.left_led, GPIO.LOW)
 
     def run_right_side(self):
         GPIO.output(self.right_led, GPIO.HIGH)
@@ -25,8 +27,13 @@ class LED_Controller:
         GPIO.output(self.right_led, GPIO.LOW)
 
     def run(self, side="both"):
-        self.run_left_side()
-        self.run_right_side()
+        if side == "left":
+            self.run_left_side()
+        elif side == "right":
+            self.run_right_side()
+        elif side == "both":
+            self.run_left_side()
+            self.run_right_side()
 
     def clean_up(self):
         # Cleanup GPIO pins
