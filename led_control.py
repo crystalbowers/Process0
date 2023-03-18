854c8d198c98cdea3870631dc9e12d1b6b348903
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 20:16:40 -0500

    Added clean up function

--- led_control.py
+++ led_control.py
@@ -8,6 +8,9 @@ class LED_Controller:
         GPIO.setup(left_led, GPIO.OUT)
         GPIO.setup(right_led, GPIO.OUT)
 
+        self.left_led = left_led
+        self.right_led = right_led
+
     def run_left_side(self):
         GPIO.output(left_led, GPIO.HIGH)
         time.sleep(3)
@@ -19,3 +22,8 @@ class LED_Controller:
     def run(self, side="both"):
         self.run_left_side()
         self.run_right_side()
+    def clean_up(self):
+        # Cleanup GPIO pins
+        self.left_led.stop()
+        self.right_led.stop()
+        GPIO.cleanup()
