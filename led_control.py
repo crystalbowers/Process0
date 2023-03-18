399e7b26b5d63192a45f52cca858c44b9262f0f1
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 20:30:01 -0500

    Fixed self variables

--- led_control.py
+++ led_control.py
@@ -4,19 +4,21 @@ import time
 class LED_Controller:
     # Initialize the class and pick your pins for each LED
     def __init__(self, left_led=7, right_led=21):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(left_led, GPIO.OUT)
-        GPIO.setup(right_led, GPIO.OUT)
-
         self.left_led = left_led
         self.right_led = right_led
 
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.left_led, GPIO.OUT)
+        GPIO.setup(self.right_led, GPIO.OUT)
+
+
+
     def run_left_side(self):
-        GPIO.output(left_led, GPIO.HIGH)
+        GPIO.output(self.left_led, GPIO.HIGH)
         time.sleep(3)
 
     def run_right_side(self):
-        GPIO.output(right_led, GPIO.HIGH)
+        GPIO.output(self.right_led, GPIO.HIGH)
         time.sleep(3)
 
     def run(self, side="both"):
