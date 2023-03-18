f0ae358970ea7d14edda6aa7476929500e2b296b
Crystal Bowers <angelbowers19@gmail.com>
2023-03-17 20:46:13 -0500

    old led code

--- clean_up.py
+++ clean_up.py
@@ -1,2 +1,19 @@
 import Jetson.GPIO as GPIO
-GPIO.cleanup()
+import time
+left_led=7
+right_led=21
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(left_led, GPIO.OUT)
+GPIO.setup(right_led, GPIO.OUT)
+x=1
+while x<5:
+        time.sleep(2)
+        GPIO.output(left_led, GPIO.HIGH)
+        GPIO.output(right_led, GPIO.HIGH)
+        print("LED IS ON")
+        time.sleep(2)
+        GPIO.output(left_led, GPIO.LOW)
+        GPIO.output(right_led, GPIO.LOW)
+        print("LED IS OFF")
+        x+=1
+        print(x)
