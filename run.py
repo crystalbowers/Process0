62b3d1744c506fde4a9a32dcff68de093bf98acb
Crystal Bowers <angelbowers19@gmail.com>
2023-03-18 16:35:15 -0500

    running diff things

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # NOTE: RIGHT NOW SEAT MOTOR CONTROL AND STEERING WHEEL CLASSES EACH TRY TO SET UP GPIO BOARD. Comment out the one you dont want if running both.
 #from seat_motor_control import Seat_MotorController
-#from steering_wheel_motor_control import Steering_Wheel_MotorController
+from steering_wheel_motor_control import Steering_Wheel_MotorController
 from led_control import LED_Controller
 # Running seat cushion
 #seat_mc = Seat_MotorController()
@@ -8,10 +8,10 @@ from led_control import LED_Controller
 #seat_mc.stop_motors()
 
 # Running Steering Wheel Motors
-#steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
-#steering_wheel_mc.run()
+steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
+steering_wheel_mc.run()
 
 steering_wheel_lc = LED_Controller()
-#steering_wheel_lc.run(side="right")
+steering_wheel_lc.run(side="right")
 steering_wheel_lc.run(side="left")
 steering_wheel_lc.clean_up()
