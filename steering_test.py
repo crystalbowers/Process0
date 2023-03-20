983af393c6f7aa78dd8ef6e966e2618eb0d12ab7
Crystal Bowers <angelbowers19@gmail.com>
2023-03-20 13:09:00 -0500

    New steering wheel motor test

--- steering_test.py
+++ steering_test.py
@@ -4,4 +4,4 @@ from steering_wheel_motor_control import Steering_Wheel_MotorController
 for i in range(4):
     steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
     steering_wheel_mc.run()
-    print("I ran")
+    print("I ran", i)
