9c0f70f8f884c75a20eb6e4cd8558321f0ac48a3
Crystal Bowers <angelbowers19@gmail.com>
2023-03-20 01:35:32 -0500

    Solution to motor delay for steering wheel

--- steering_wheel_motor_control.py
+++ steering_wheel_motor_control.py
@@ -8,20 +8,21 @@ class Steering_Wheel_MotorController:
         # Initalize I2C bus and DRV2065 module
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.drv = adafruit_drv2605.DRV2605(self.i2c)
+
         # Set effect number
         self.effect_num = effect_num
+
+        # Set the sequence
+        self.drv.sequence[0] = adafruit_drv2605.Effect(self.effect_num)
+
         # Need to do this or else there's a 10 sec delay on motor every time
         self.run(run_time=0)
 
     def run(self,run_time=5):
-        # Run vibrations
-        self.drv.sequence[0] = adafruit_drv2605.Effect(self.effect_num)
-
         # Run for five seconds then stop
         self.drv.play()
         time.sleep(run_time)
         self.drv.stop()
-
 
 if __name__ == "__main__":
     s = Steering_Wheel_MotorController()
