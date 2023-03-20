1ee4943060319396db9086fcb072900578937f32
Crystal Bowers <angelbowers19@gmail.com>
2023-03-20 00:24:01 -0500

    real time steering wheel motor

--- steer.py
+++ steer.py
@@ -8,17 +8,18 @@ drv = adafruit_drv2605.DRV2605(i2c)
 # Start real-time playback
 drv.realtime_value = 0
 drv.mode = adafruit_drv2605.MODE_REALTIME
-print(drv.mode)
+print("mode is",drv.mode)
 
 # Buzz the motor briefly at 50% and 100% amplitude
 drv.realtime_value = 64
 time.sleep(0.5)
-#drv.realtime_value = 127
-#time.sleep(0.5)
+drv.realtime_value = 127
+time.sleep(0.5)
 
 # Stop real-time playback
-#drv.realtime_value = 0
+drv.realtime_value = 0
 drv.mode = adafruit_drv2605.MODE_INTTRIG
+print("mode is",drv.mode)
 
 
 
