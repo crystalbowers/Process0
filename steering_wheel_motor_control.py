bd48198778a2f4e92e8846298e3a8b5fed50f1cb
Crystal Bowers <angelbowers19@gmail.com>
2023-03-20 13:17:12 -0500

    New steering wheel motor test

--- steering_wheel_motor_control.py
+++ steering_wheel_motor_control.py
@@ -28,5 +28,5 @@ class Steering_Wheel_MotorController:
 if __name__ == "__main__":
     s = Steering_Wheel_MotorController()
     # test if it works
+    s.run(run_time=1)
     s.run(run_time=5)
-    s.run()
