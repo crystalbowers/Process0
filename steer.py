b744fc2542dc489a8f4ce0e949742c30f168d38b
Crystal Bowers <angelbowers19@gmail.com>
2023-03-20 00:19:08 -0500

    real time steering wheel motor

--- steer.py
+++ steer.py
@@ -2,15 +2,16 @@ import board
 import busio
 import adafruit_drv2605
 import time
-i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
 drv = adafruit_drv2605.DRV2605(i2c)
+
 # Start real-time playback
 drv.realtime_value = 0
 drv.mode = adafruit_drv2605.MODE_REALTIME
 
 # Buzz the motor briefly at 50% and 100% amplitude
-drv.setRealtimeValue=64
-#drv.realtime_value = 64
+#drv.setRealtimeValue=64
+drv.realtime_value = 64
 time.sleep(0.5)
 #drv.realtime_value = 127
 #time.sleep(0.5)
