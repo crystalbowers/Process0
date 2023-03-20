d94df12be89dd88d28000a704343e990183ea4bd
Crystal Bowers <angelbowers19@gmail.com>
2023-03-20 00:30:04 -0500

    real time steering wheel motor

--- steer.py
+++ steer.py
@@ -12,9 +12,11 @@ print("mode is",drv.mode)
 
 # Buzz the motor briefly at 50% and 100% amplitude
 drv.realtime_value = 64
-time.sleep(0.5)
+print("please run")
+time.sleep(2)
 drv.realtime_value = 127
-time.sleep(0.5)
+print("did i go")
+time.sleep(3)
 
 # Stop real-time playback
 drv.realtime_value = 0
