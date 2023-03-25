5f862aac895dd4b595a3cb14c02b5bf9872fb469
Crystal Bowers <angelbowers19@gmail.com>
2023-03-25 14:50:34 -0500

    Changed to using channels 2 and 7 of multiplexer

--- multiplexer.py
+++ multiplexer.py
@@ -13,17 +13,17 @@ i2c = busio.I2C(board.SCL, board.SDA)
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
 # For each motor driver, create it using the TCA9548A channel instead of the I2C object
-tsl1 = adafruit_drv2605.DRV2605(tca[0])
-#tsl2 = adafruit_drv2605.DRV2605(tca[7])
+tsl1 = adafruit_drv2605.DRV2605(tca[2])
+tsl2 = adafruit_drv2605.DRV2605(tca[7])
 
 tsl1.sequence[0] = adafruit_drv2605.Effect(effect_num)
-#tsl2.sequence[0] = adafruit_drv2605.Effect(effect_num)
+tsl2.sequence[0] = adafruit_drv2605.Effect(effect_num)
 
 tsl1.play()
 time.sleep(run_time)
 tsl1.stop()
 
-#tsl2.play()
-#time.sleep(run_time)
-#tsl2.stop()
+tsl2.play()
+time.sleep(run_time)
+tsl2.stop()
 
