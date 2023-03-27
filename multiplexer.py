ca6e3296e20a726fe9178327ee8107777897c756
Crystal Bowers <angelbowers19@gmail.com>
2023-03-27 11:28:02 -0500

    Multiplexer steering code added channels as parameters

--- multiplexer.py
+++ multiplexer.py
@@ -14,16 +14,16 @@ tca = adafruit_tca9548a.TCA9548A(i2c)
 
 # For each motor driver, create it using the TCA9548A channel instead of the I2C object
 tsl1 = adafruit_drv2605.DRV2605(tca[0])
-#tsl2 = adafruit_drv2605.DRV2605(tca[7])
+tsl2 = adafruit_drv2605.DRV2605(tca[1])
 
 tsl1.sequence[0] = adafruit_drv2605.Effect(effect_num)
-#tsl2.sequence[0] = adafruit_drv2605.Effect(effect_num)
+tsl2.sequence[0] = adafruit_drv2605.Effect(effect_num)
 
 tsl1.play()
 time.sleep(run_time)
 tsl1.stop()
 
-'''tsl2.play()
+tsl2.play()
 time.sleep(run_time)
-tsl2.stop()'''
+tsl2.stop()
 
