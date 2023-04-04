c125eec10f71d971e43f090c4f339b99d5cf1534
Crystal Bowers <angelbowers19@gmail.com>
2023-04-04 09:53:18 -0500

    Updated channels

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from seat_motor_control import Seat_MotorController
 from led_control import LED_Controller
 # Running seat cushion
 seat_mc = Seat_MotorController()
-seat_mc.run(side="both", delay_secs=10)
+seat_mc.run(side="left", delay_secs=10)
 seat_mc.stop_motors()
 
 # Running Steering Wheel Motors
