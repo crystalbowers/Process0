6fe7cb54932c830b1c06cd2ada2d84149ac1f3e1
Crystal Bowers <angelbowers19@gmail.com>
2023-04-12 18:25:47 -0500

    restoring o.g. motorcode pwm from earlier today

--- seat_motor_control.py
+++ seat_motor_control.py
@@ -69,7 +69,7 @@ class Seat_MotorController:
     def run_right_side(self, duty_cycle):
         self.set_motor_duty_cycle(duty_cycle, self.motor2_in3, self.motor2_in4)
 
-    def run(self, side="both", delay_secs=5, duty_cycle=50):
+    def run(self, side="left", delay_secs=5, duty_cycle=50):
         if side == "left":
             self.run_left_side(duty_cycle)
             print("Ran left")
