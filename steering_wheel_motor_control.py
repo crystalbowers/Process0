ad5d31223425589f7481a77dd98a58f68132f1ae
Crystal Bowers <angelbowers19@gmail.com>
2023-04-13 13:55:28 -0500

    Added final steering wheel code for Orin

--- steering_wheel_motor_control.py
+++ steering_wheel_motor_control.py
@@ -5,7 +5,8 @@ import time
 
 class Steering_Wheel_MotorController:
     def __init__(self, effect_num=118):
-        # Initalize I2C buses and DRV2065 modules
+        # Initalize I2C buses (7 and 1) and DRV2065 modules
+        # i2c1 and drv1 control the left side, while i2c2 and drv2 control the right side
         self.i2c1 = busio.I2C(board.SCL, board.SDA)
         self.i2c2 = busio.I2C(board.SCL_1, board.SDA_1)
         self.drv1 = adafruit_drv2605.DRV2605(self.i2c1, address=0x5A)
@@ -20,17 +21,35 @@ class Steering_Wheel_MotorController:
 
         # Need to do this or else there's a 10 sec delay on motor every time
         # 10 sec delay will only occur once during initialization and not when it runs after that
-        self.run(run_time=0)
+        self.run_left_side(run_time=0)
+        self.run_right_side(run_time=0)
 
-
-    def run(self,run_time=5):
-        # Run for five seconds then stop
+    def run_left_side(self, run_time=3):
         self.drv1.play()
         time.sleep(run_time)
         self.drv1.stop()
+
+    def run_right_side(self,run_time=3):
         self.drv2.play()
         time.sleep(run_time)
         self.drv2.stop()
+
+    def run_both_sides(self, run_time=3):
+        self.drv1.play()
+        time.sleep(run_time)
+        self.drv1.stop()
+
+        self.drv2.play()
+        time.sleep(run_time)
+        self.drv2.stop()
+    def run(self,side="both",run_time=3):
+        # Run for three seconds then stop
+        if side == "both":
+            self.run_both_sides(run_time)
+        elif side == "left":
+            self.run_left_side(run_time)
+        elif side == "right":
+            self.run_right_side(run_time)
 
 if __name__ == "__main__":
     s = Steering_Wheel_MotorController()
