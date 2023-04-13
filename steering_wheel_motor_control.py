f2738ea3faa62ec9b44d4bc307632a7d592eca9a
Crystal Bowers <angelbowers19@gmail.com>
2023-04-13 00:02:05 -0500

    commented in steering wheel in run.py

--- steering_wheel_motor_control.py
+++ steering_wheel_motor_control.py
@@ -5,19 +5,23 @@ import time
 
 class Steering_Wheel_MotorController:
     def __init__(self, effect_num=118):
-        # Initalize I2C bus and DRV2065 module
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.drv = adafruit_drv2605.DRV2605(self.i2c)
+        # Initalize I2C buses and DRV2065 modules
+        self.i2c1 = busio.I2C(board.SCL, board.SDA)
+        self.i2c2 = busio.I2C(board.SCL_1, board.SDA_1)
+        self.drv1 = adafruit_drv2605.DRV2605(self.i2c1, address=0x5A)
+        self.drv2 = adafruit_drv2605.DRV2605(self.i2c2, address=0x5A)
 
         # Set effect number
         self.effect_num = effect_num
 
         # Set the sequence
-        self.drv.sequence[0] = adafruit_drv2605.Effect(self.effect_num)
+        self.drv1.sequence[0] = adafruit_drv2605.Effect(self.effect_num)
+        self.drv2.sequence[0] = adafruit_drv2605.Effect(self.effect_num)
 
         # Need to do this or else there's a 10 sec delay on motor every time
         # 10 sec delay will only occur once during initialization and not when it runs after that
         self.run(run_time=0)
+
 
     def run(self,run_time=5):
         # Run for five seconds then stop
