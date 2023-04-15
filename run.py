67661c8567109a75cbf48d732cb2ec1097311e39
Crystal Bowers <angelbowers19@gmail.com>
2023-04-15 15:38:00 -0500

    Increased duty cycle for right side

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from orin_seat import Seat_MotorController
 #from led_control import LED_Controller
 # Running seat cushion
 seat_mc = Seat_MotorController()
-seat_mc.run(side="right")
+seat_mc.run(side="left")
 #seat_mc.stop_motors()
 #seat_mc.clean_up()
 
