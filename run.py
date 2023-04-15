f4c8cf2145054854450e89758e51da11f3848ebe
Crystal Bowers <angelbowers19@gmail.com>
2023-04-15 15:50:34 -0500

    Moved code and got deleted multiplexer code from requirements.txt. Turned on steering wheel in run.

--- run.py
+++ run.py
@@ -1,23 +1,15 @@
 # NOTE: RIGHT NOW SEAT MOTOR CONTROL AND STEERING WHEEL CLASSES EACH TRY TO SET UP GPIO BOARD.
 # Comment out the one you dont want if running both.
 #from seat_motor_control import Seat_MotorController
-from orin_seat import Seat_MotorController
-#from steering_wheel_motor_control import Steering_Wheel_MotorController
-#from multiplexer_steering_code import Steering_Wheel_MotorController
-#from led_control import LED_Controller
+#from orin_seat import Seat_MotorController
+from steering_wheel_motor_control import Steering_Wheel_MotorController
+
 # Running seat cushion
-seat_mc = Seat_MotorController()
-seat_mc.run(side="left")
-#seat_mc.stop_motors()
-#seat_mc.clean_up()
+#seat_mc = Seat_MotorController()
+#seat_mc.run(side="left")
 
 # Running Steering Wheel Motors
-#steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
-#steering_wheel_mc.run()
-#print("I ran")
+steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
+steering_wheel_mc.run()
+print("I ran")
 
-# Running LED lights
-#steering_wheel_lc = LED_Controller()
-#steering_wheel_lc.run(side="right")
-#steering_wheel_lc.run(side="left")
-#steering_wheel_lc.clean_up()
