ff9a35cef56a799c72f44a6d9b21248ed273fec4
Crystal Bowers <angelbowers19@gmail.com>
2023-04-15 15:36:00 -0500

    Changed run to left side

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ from orin_seat import Seat_MotorController
 #from led_control import LED_Controller
 # Running seat cushion
 seat_mc = Seat_MotorController()
-seat_mc.run(side="right")
+seat_mc.run(side="left")
 #seat_mc.stop_motors()
-seat_mc.clean_up()
+#seat_mc.clean_up()
 
 # Running Steering Wheel Motors
 #steering_wheel_mc = Steering_Wheel_MotorController(effect_num=118)
